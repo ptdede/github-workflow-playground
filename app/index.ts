f3dc8dfc5e205f71fc8486b358a4f7083d243cc3
Dede Adhyatmika <dede.adhyatmika@bfi.co.id>
2024-02-21 00:50:55 +0800

    chore: pull request pipeline
    chore: add PR request template
    
    feat: testing PR request
    
    fix: lint
    
    feat: add usecase

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,18 @@
+import lodash from "lodash";
+
 const runApp = () => {
   // Uncomment this will make eslint error.
   // console.log("This will error on PR check");
+  const nestedObj = {
+    depth1: {
+      depth2: {
+        value: "value",
+      },
+    },
+  };
+  const value = lodash.get(nestedObj, "depth1.depth2.value");
+
+  return value;
 };
 
 runApp();
